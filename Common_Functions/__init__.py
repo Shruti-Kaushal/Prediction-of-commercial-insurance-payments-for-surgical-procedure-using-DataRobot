f746967aadc935a415f58fa6958065d7cc7be07a
Shruti-Kaushal <shrutikaushal95@gmail.com>
2022-11-22 15:38:16 -0500

    adding optimal k function to KNN functions and packaging it as a module

--- Common_Functions/__init__.py
+++ Common_Functions/__init__.py
@@ -2,5 +2,5 @@ from .processing_functions import data_split
 from .processing_functions import add_unique_identifier
 from .processing_functions import data_cleaning
 from .processing_functions import hospital_data_agg
-from KNN_function import knn_func
-from .optimal_K_func import optimal_k
+from .KNN_functions import knn_func
+from .KNN_functions import optimal_k
