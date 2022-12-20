0d181af18c92df99f615c8f2bffc924318630648
Ryan Rogers <93682511+rjr2168@users.noreply.github.com>
2022-12-20 10:32:43 -0500

    Cleanup and compartmentalization in progress...

--- Common_Functions/KNN_functions.py
+++ Common_Functions/KNN_functions.py
@@ -1,34 +1,59 @@
+from .processing_functions import standardize_data
+from sklearn.impute import KNNImputer
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_percentage_error
 
-def knn_func(data, neighbours = 3):
-    from sklearn.impute import KNNImputer
-    from sklearn.preprocessing import MinMaxScaler
-    import numpy as np
-    import pandas as pd
-    ## replace all the infinite values with 0's in the data
+def knn_func(data: pd.DataFrame,
+             neighbours: int = 3) -> pd.DataFrame:
+    """
+    This function is a helper method defined in order to help determine our optimal number of clusters.
+    
+    Args:
+    data: (pandas: DataFrame) - a pandas data frame to be imputed on
+    neighbors: (int) - number of neighbors to use for data imputation
+    
+    Returns:
+    imputed_values (pandas: DataFrame) - data frame after knn imputation has been performed on it
+    """
+    # Replace all the infinite values with 0's in the data
     data.replace([np.inf, -np.inf], 0, inplace=True)
     
+    # Initialize imputer
     imputer = KNNImputer(n_neighbors=neighbours)
     
-    ## apply the min max scaler to standardize the data
+    # Apply the min max scaler to standardize the data
     scaler = MinMaxScaler()
     X_df = pd.DataFrame(scaler.fit_transform(data), columns = data.columns)
     imputed_values = imputer.fit_transform(X_df.values)
     imputed_values = pd.DataFrame(scaler.inverse_transform(imputed_values),columns= data.columns)
+    
     return imputed_values
 
-## pass the processed data to this function with catgorical variables One Hot Encoded
-## we recognized a pattern where the train and test MAPE attain minima at the same K value, hence we take np.argmin of the train_mape only 
-def optimal_k(data):
+def optimal_k(data: pd.DataFrame) -> int:
+    """
+    This function determines the optimal number of clusters, k for KNN imputation.
+    We observed that the training data generally gave the optimal number of clusters, so we
+    consider only training data in our determination.
+    
+    Args:
+    data: (pandas: DataFrame) - a pandas data frame to be assessed for optimal clustering
+    
+    Returns:
+    (int) - Our optimal number of clusters for KNN imputation
+    """
+    # Set configuration
     RDM_SEED = 123
     TRAIN_TEST_PROPORTION = 0.8
+    
+    # Initialize lists
     train_mape = []
     test_mape = []
-    # from KNN_function import knn_func
-    import numpy as np
-    from sklearn.model_selection import train_test_split
-    from sklearn.ensemble import RandomForestRegressor
-    from sklearn.metrics import mean_absolute_percentage_error
 
+    # Test various clusterings on a random forest model
     k_list = [2,3,4,5,6,7,8,10,12,14,16]
     for x in k_list:
         model_imputed = knn_func(data,neighbours=x)
@@ -45,4 +70,30 @@ def optimal_k(data):
         y_test_pred_imp = baseline_model_imp.predict(X_test_imp)
         train_mape.append(mean_absolute_percentage_error(y_true=y_train_imp, y_pred=y_train_pred_imp))
         test_mape.append(mean_absolute_percentage_error(y_true=y_test_imp, y_pred=y_test_pred_imp))
+    
+    # Return k that yielded the lowest training MAPE
     return k_list[np.argmin(train_mape)]
+
+def impute_knn(train_data: pd.DataFrame,
+               val_data: pd.DataFrame,
+               optimal_k: int) -> (pd.DataFrame,pd.DataFrame):
+    """
+    This function performs knn imputation on our whole dataset, using the number of neighbors specified.
+    
+    Args:
+    train_data: (pandas: DataFrame) - a pandas data frame used to train our imputer. It is then imputed
+    val_data: (pandas: DataFrame) - a pandas data frame to be imputed only
+    
+    Returns:
+    train_data_scaled, val_data_scaled (pd.DataFrame,pd.DataFrame) - Our newly imputed datasets
+    """
+    # Standardize datasets
+    train_data_scaled, val_data_scaled = standardize_data(train_data, val_data)
+    
+    # Initialize imputer, then perform imputation
+    knn = KNNImputer(n_neighbors = optimal_k)
+
+    train_data_scaled[list(train_data_scaled.columns)] = knn.fit_transform(train_data_scaled)
+    val_data_scaled[list(val_data_scaled.columns)] = knn.transform(val_data_scaled)
+    
+    return train_data_scaled, val_data_scaled
--- Common_Functions/processing_functions.py
+++ Common_Functions/processing_functions.py
@@ -1,25 +1,35 @@
-def data_split(data, count_col_name = 'priv_count', count_thresh = 50):
+from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
+    
+def data_split(data: pd.DataFrame,
+               count_col_name: str = 'priv_count',
+               count_thresh: str = 50) -> (pd.DataFrame, pd.DataFrame):
     """
     This function splits the data into model set and future set. Model set is the data used to train, evaluate
-    test the model. Future data is what the model needs to predict on.
+    test the model. Future data is data that the model needs to predict on exclusively.
     
     Args:
-    data (pandas: DataFrame) - a pandas data frame with at least 3 columns - "priv_pay_mean", "priv_pay_median" and count_col_name
-    count_col_name (str) - name of the column which is thresholded to make the split
-    count_thresh (int) - threshold value used to split data on count_col_name
+    data: (pandas: DataFrame) - a pandas data frame with at least 3 columns - "priv_pay_mean", "priv_pay_median" and count_col_name
+    count_col_name: (str) - name of the column which is thresholded to make the split
+    count_thresh: (int) - threshold value used to split data on count_col_name
     
     Returns:
     model_data (pandas: DataFrame) - data frame with observations that will be used to train and test model
     future_data (pandas: DataFrame) - data frame with all observations on which model will make predictions
     """
+    # Filter out rows where the target is invalid
     data = data[(data['priv_pay_median'] > 0) | (data['priv_pay_median'].isnull())]
+    # Isolate rows below our specified threshold or null
     future_data = data[(data[count_col_name] <= count_thresh) | (data[count_col_name].isnull())]
+    # Isolate rows above our specified threshold
     model_data = data[data[count_col_name] > count_thresh]
     model_data = model_data[model_data.priv_pay_median.notnull()]
+    
     return model_data, future_data
 
 
-def add_unique_identifier(data, id_col_list=['msa','year','site','group']):
+def add_unique_identifier(data: pd.DataFrame,
+                          id_col_list: list = ['msa','year','site','group']) -> pd.DataFrame:
     """
     Adds a unique identifier to the data which is constructed by joining the entries in the columns of col_list
     with a '_' separator.
@@ -38,10 +48,13 @@ def add_unique_identifier(data, id_col_list=['msa','year','site','group']):
     cols = data_new.columns.to_list()
     cols = cols[-1:] + cols[:-1]
     data_new = data_new[cols]
+    
     return data_new
 
 
-def data_cleaning(data, one_hot = True, dropna = True):
+def data_cleaning(data: pd.DataFrame,
+                  one_hot: bool = True,
+                  dropna: bool = True) -> pd.DataFrame:
     """
     Applies preprocessing to our dataset for ease of use in model building
     
@@ -52,17 +65,18 @@ def data_cleaning(data, one_hot = True, dropna = True):
     
     Returns:
     data_new (pandas: DataFrame) - a transformed data frame
-    """
-    import pandas as pd
-    
+    """    
     data_new = data
     if one_hot:
+        # One hot encoding
         data_new = pd.get_dummies(data_new)
     if dropna:
+        # Drop all NAs across columns
         data_new = data_new.dropna()
+    
     return data_new
 
-def hospital_data_agg(data):
+def hospital_data_agg(data: pd.DataFrame) -> pd.DataFrame:
     """
     Applies aggregation to our hospital dataset for merging with our dataset
     
@@ -72,18 +86,15 @@ def hospital_data_agg(data):
     Returns:
     hospital_msa (pandas: DataFrame) - an aggregated data frame
     """
-    
-    import pandas as pd
-    
     hospital_data = data
     
+    # Prepare for aggregated columns
     hospital_data["Is_Teaching"] = (hospital_data["teaching"] == "YES").astype(int)
     hospital_data["Beds_Over_500"] = (hospital_data["beds_grp"] == "500+").astype(int)
     hospital_data["Is_Urban"] = (hospital_data["urban_rural"] == "URBAN").astype(int)
     hospital_data["Is_Private"] = ((hospital_data["ownership"] == "PRIVATE (NOT FOR PROFIT)") |
                                    (hospital_data["ownership"] == "PRIVATE (FOR PROFIT)")).astype(int)
-
-
+    # Create aggregated columns
     hospital_msa = hospital_data.groupby("MSA_CD").agg({"prvdr_num": "count",
                                                         "Is_Teaching":"mean",
                                                         "Beds_Over_500":"mean",
@@ -91,6 +102,7 @@ def hospital_data_agg(data):
                                                         "Is_Private":"mean"
     })
     hospital_msa.reset_index(inplace=True)
+    # Rename newly aggregated columns
     hospital_msa.rename(columns = {"MSA_CD":"msa",
                                    "prvdr_num": "Hospitals",
                                    "Is_Teaching":"PctTeaching",
@@ -99,4 +111,45 @@ def hospital_data_agg(data):
                                    "Is_Private":"PctPrivate"
                                   },
                         inplace=True)
+    
     return hospital_msa
+
+
+# Method from Shruti's code
+def standardize_data(train_data: pd.DataFrame,
+                     val_data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Standardize training and validation (or test) data using the training data to prepare a MinMaxScaler.
+    
+    Args:
+    train_data (pandas: DataFrame) - a pandas data frame containing our training data  
+    val_data (pandas: DataFrame) - a pandas data frame containing our validation (or test) data  
+    
+    Returns:
+    train_data_scaled (pandas: DataFrame) - a pandas data frame containing our scaled training data  
+    val_data_scaled (pandas: DataFrame) - a pandas data frame containing our scaled validation (or test) data 
+    """
+    # Drop variables that do not make sense to standardize (categories and coordinates)
+    train_temp = train_data.drop(columns = ['site','cluster','lat','lon'])
+    val_temp = val_data.drop(columns = ['site','cluster','lat','lon'])
+    
+    # Initialize scaler
+    scaler = MinMaxScaler()
+    
+    # Train scaler, scale training data, re-append columns
+    train_data_scaled = scaler.fit_transform(train_temp)
+    train_data_scaled = pd.DataFrame(train_data_scaled, columns = train_temp.columns)
+    train_data_scaled['cluster'] = train_data['cluster'].to_list()
+    train_data_scaled['site'] = train_data['site'].to_list()
+    train_data_scaled['lat'] = train_data['lat'].to_list()
+    train_data_scaled['lon'] = train_data['lon'].to_list()
+    
+    # Scale validation data, re-append columns
+    val_data_scaled = scaler.transform(val_temp)
+    val_data_scaled = pd.DataFrame(val_data_scaled, columns = val_temp.columns)
+    val_data_scaled['cluster'] = val_data['cluster'].to_list()
+    val_data_scaled['site'] = val_data['site'].to_list()
+    val_data_scaled['lat'] = val_data['lat'].to_list()
+    val_data_scaled['lon'] = val_data['lon'].to_list()
+    
+    return train_data_scaled, val_data_scaled
